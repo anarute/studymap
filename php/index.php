012a5f94fafe27d3e9c1a3e79c2b1ed18f6990cb
Joao Santos <jtadeusantos@gmail.com>
2012-12-03 17:57:38 -0200

    Index modify for test

--- php/index.php
+++ php/index.php
@@ -1,9 +1,26 @@
 <?php
-
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * file: facebook_login.php
+ * 
+ * Description: Make Facebook login with Facebook SDK
+ * 
+ * Author: Joao Santos
  */
 
-echo "Correto";
+require_once ("nbproject/facebook-php-sdk/src/facebook.php");
+
+$config = array();
+
+$config['appID'] = "173457916129382";
+$config['secret'] = "c6cd4dfffdc3cfd8e7ab17ead66002a8";
+$config['fileUpload'] = true;
+
+$facebook = new Facebook($config);
+
+$user = $facebook->getUser();
+
+echo "Get User ID:" . $user;
+
+
 ?>
+
